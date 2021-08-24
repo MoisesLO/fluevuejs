b1cd0682522931dcb4983b0a829d0ee7d3dab11c
moises <elnaufrago2009@gmail.com>
2021-08-24 17:23:56 -0500

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+var data;
+List _items = [];
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Vue js',
-      home: Home(),
-    )
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Vue js',
+    home: Home(),
+  ));
 }
 
 class Home extends StatefulWidget {
@@ -18,16 +21,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // import Json
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/json/vue.json');
+    final datos = await json.decode(response);
+    setState(() {
+      _items = datos;
+      data = datos;
+      print(datos);
+    });
+  }
+
   @override
+  void initState() {
+    readJson();
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Vue Js'),
       ),
-      body: Center(
-        child: Text('Moises'),
-      ),
+      body: ListView.builder(
+          itemCount: _items.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 22,
+                  child: Icon(Icons.anchor),
+                ),
+                title: Text('${_items[index]['title']}'),
+              ),
+            );
+          }),
     );
   }
 }
-
